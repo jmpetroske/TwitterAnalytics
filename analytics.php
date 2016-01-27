80d2433a65b58bf01f571242e05bda1b74deb0e6
jmpetroske <jmpetroske@gmail.com>
2016-01-26 22:41:54 -0800

    Added retweet count

---
+++ analytics.php
@@ -0,0 +1,56 @@
+<?php
+	error_reporting(E_ALL);
+	ini_set("display_errors", "1");
+	
+	if(!isset($_GET["handle"]) || !isset($_GET["tweetMultiplier"])){
+		header("HTTP/1.1 422");
+		die("required GET parameter missing");
+	}
+
+	$MAX_TWEET_MULTIPLIER = 15;
+	$tweetMultiplier = $_GET["tweetMultiplier"];
+	if ($tweetMultiplier < 1){
+		$tweetMultiplier = 1;
+	} elseif ($tweetMultiplier > $MAX_TWEET_MULTIPLIER){
+		$tweetMultiplier = $MAX_TWEET_MULTIPLIER;
+	}
+	$handle = $_GET["handle"];		
+
+	require("twitteroauth/autoload.php");
+	use Abraham\TwitterOAuth\TwitterOAuth; 
+	
+					
+	/* authinfo.json MUST have twitter api credentals stored in it */
+	$authinfo = json_decode(file_get_contents("authinfo.json"));
+	$connection = new TwitterOAuth($authinfo->CONSUMER_KEY,
+					$authinfo->CONSUMER_SECRET,
+					$authinfo->accessToken,
+					$authinfo->accessTokenSecret);
+	$query = array("screen_name" => $handle, "count" => "200");
+	$count = 0;
+	$timestamps = array();
+//	print_r($connection->get("statuses/user_timeline", $query));
+	do {
+		$results = $connection->get("statuses/user_timeline", $query);
+		if (count($results) == 0){
+			break;
+		}
+		$minID = $results[0]->id; 
+        foreach($results as $status){
+			$data = new stdClass();
+			$data->time = strtotime($status->created_at);
+			if (property_exists($status, "retweeted_status")) {
+				$data->retweets = 0;
+			} else {
+				$data->retweets = $status->retweet_count;
+			}
+            $timestamps[] = $data;
+			if ($status->id < $minID){
+				$minID = $status->id;
+			}
+        }
+		$count++;
+		$query["max_id"] = $minID - 1;
+	} while (/*$count < $tweetMultiplier*/0==1); 
+	print(json_encode($timestamps));
+?>
